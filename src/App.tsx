2b105ff07afb9facff5a21fd5d97ed60cbf376fe
Mitul <dev.mitul17@gmail.com>
2022-11-01 15:32:40 +0530

    initial commit for HRMS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+function App() {
+  return (
+    <Fragment>
+      <h1>HRMS</h1>
+      <h1>We are coming soon.</h1>
+    </Fragment>
+  );
+}
+
+export default App;
